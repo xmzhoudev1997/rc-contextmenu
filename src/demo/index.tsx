6e87e4d7441d1693cb84a6e301453aceb4f579ab
xmzhoudev1997 <165015145+xmzhoudev1997@users.noreply.github.com>
2024-04-03 09:59:52 +0800

    更新组件文档

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,51 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.less';
 import { RCContextMenu } from '@xmzhou/rc-contextmenu';
 
 const Index = () => {
+    const [visible, setVisble] = useState(false);
     return (
-        <RCContextMenu
-            onChange={(k) => {
-                console.log(k);
-            }}
-            menu={[
-                {
-                    key: 'format',
-                    label: '格式化',
-                    shortcutKeys: ['command', 'F']
-                },
-                {
-                    key: 'run',
-                    label: '运行',
-                    shortcutKeys: ['command', 'Enter']
-                },
-                {
-                    key: 'divider1',
-                    type: 'divider',
-                },
-                {
-                    key: 'copy',
-                    label: '复制',
-                    shortcutKeys: ['command', 'C']
-                },
-                {
-                    key: 'cut',
-                    label: '剪贴',
-                    shortcutKeys: ['command', 'X']
-                },
-                {
-                    key: 'paste',
-                    label: '粘贴',
-                    shortcutKeys: ['command', 'V']
-                },
-            ]}
-        >
-            <div className="demo-container" tabIndex={0}>
-                <div className="demo-content">
+        <div className="demo-container" onScroll={() => setVisble(false)}>
+            <RCContextMenu
+                visible={visible}
+                onVisibleChange={setVisble}
+                onChange={(k) => {
+                    console.log('触发菜单', k);
+                }}
+                menu={[
+                    {
+                        key: 'format',
+                        label: '格式化',
+                        shortcutKeys: ['command', 'F']
+                    },
+                    {
+                        key: 'run',
+                        label: '运行',
+                        shortcutKeys: ['command', 'Enter']
+                    },
+                    {
+                        key: 'divider1',
+                        type: 'divider',
+                    },
+                    {
+                        key: 'copy',
+                        label: '复制',
+                        shortcutKeys: ['command', 'C']
+                    },
+                    {
+                        key: 'cut',
+                        label: '剪贴',
+                        shortcutKeys: ['command', 'X']
+                    },
+                    {
+                        key: 'paste',
+                        label: '粘贴',
+                        shortcutKeys: ['command', 'V']
+                    },
+                ]}
+            >
+                <div className="demo-content" tabIndex={0}>
                     我是内容区域
                 </div>
-            </div>
-        </RCContextMenu>
+            </RCContextMenu>
+        </div>
     );
 };
 
